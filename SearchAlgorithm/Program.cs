c0ff70fee72519e7b8f314ee8b2bb21c5fc74a86
ZaqyMunif <zaqymunif17@gmail.com>
2022-10-28 16:14:03 +0700

    add method input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -27,7 +27,17 @@ namespace SearchAlgorithm
                 else
                     Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements.\n");
             }
-
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("  Enter array element  ");
+            Console.WriteLine("-----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
     }
 }
